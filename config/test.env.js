af654b5e6a47a7425c76b9442a3b56fd0cf079cf
caesar chan <caeasar_chan@live.com>
2019-03-30 12:24:02 +0800

    update

--- config/test.env.js
+++ config/test.env.js
@@ -8,5 +8,5 @@ module.exports = merge(devEnv, {
   LOGIN_URL: '"http://ad.newgls.cn/login"',
   SERVER_URL: '"http://papi.newgls.cn"',
   HTTPS_SERVER_URL: '"https://papi.newgls.cn"',
-  CDN_URL: "'http://cdn.exe666.com/'"
+  CDN_URL: "'http://cdn.xingstation.cn/'"
 })
