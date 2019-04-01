5b02c9295d0cb8c964f48e342a82a5a69c66e263
Joy Wang <joywangx@sina.com>
2019-04-01 16:50:44 +0800

    更换cdn

--- config/test.env.js
+++ config/test.env.js
@@ -8,5 +8,5 @@ module.exports = merge(devEnv, {
   LOGIN_URL: '"http://ad.xingstation.net/login"',
   SERVER_URL: '"http://papi.xingstation.net"',
   HTTPS_SERVER_URL: '"https://papi.xingstation.net"',
-  CDN_URL: "'http://cdn.exe666.com/'"
+  CDN_URL: "'http://cdn.xingstation.cn/'"
 })
